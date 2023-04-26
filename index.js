821d63470db3383251b8f6495a7938eebcbbf111
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-04-26 01:35:08 -0700

    added new role function

--- index.js
+++ index.js
@@ -138,3 +138,46 @@ const addDepartment = () => {
         })
     })
 };
+
+
+// add role to role table function
+const addRole = () => {
+    const query = `SELECT * FROM role; SELECT * FROM department`
+    db.query(query, (err, newRole) => {
+        if (err) throw err;
+
+        console.table(chalk.yellow('List of current roles'), newRole);
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newTitle',
+                message: 'Enter new role title:'
+            },
+            {
+                type: 'input',
+                name: 'newSalary',
+                message: 'Enter salary amount:'
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: 'Select department for the role:',
+                choices: [
+                    { name: 'Engineering', value: 1 },
+                    { name: 'Finance', value: 2 },
+                    { name: 'Legal', value: 3 },
+                    { name: 'Sale', value: 4 },
+                ]
+            }
+        ]).then((answer) => {
+            db.query(
+                `INSERT INTO role(title, department_id, salary) 
+                VALUES ( ?, ?, ?)
+                ('${answer.newTitle}', '${answer.newSalary}',
+                (SELECT id FROM department WHERE department_id = '${answer.department}'));`
+            )
+            mainMenu();
+        })
+    })
+}
