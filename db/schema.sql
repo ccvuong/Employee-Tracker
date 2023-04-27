b61c1ea2d911b6d4e4d4d451697e164d92c9b801
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-04-26 22:13:25 -0700

    fixed schema.sql errors

--- db/schema.sql
+++ db/schema.sql
@@ -17,8 +17,7 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
 
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 --  employee table
@@ -26,13 +25,11 @@ CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NO NULL,
+    role_id INT NOT NULL,
     manager_id INT,
 
-    FOREIGN KEY (role_id),
-    FOREIGN KEY (manager_id),
-    REFERENCES (role_id),
-    REFERENCES (manager_id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
 
-    ON DELETE SET NULL
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+
 );
